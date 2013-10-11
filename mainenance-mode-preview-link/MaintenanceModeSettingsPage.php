46d8d4445e2d4082b021b284fb026856a37d08fd
nebbens <nebbens@ebbensweb.com>
2013-10-11 00:23:24 -0500

    Created plugin!

---
+++ mainenance-mode-preview-link/MaintenanceModeSettingsPage.php
@@ -0,0 +1,134 @@
+<?php
+class MaintenanceModeSettingsPage
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'page_init' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Maintenace Mode Settings Admin', 
+            'Maint Mode Settings', 
+            'manage_options', 
+            'maintenance_mode-setting-admin', 
+            array( $this, 'create_admin_page' )
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'maint_display_text' );
+        ?>
+        <div class="wrap">
+            <?php screen_icon(); ?>
+            <h2>Maintenance Mode Settings</h2>           
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'maint_display_settings' );  //option group 
+                do_settings_sections( 'maintenance_mode-setting-admin' );
+                submit_button(); 
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init()
+    {        
+        register_setting(
+            'maint_display_settings', // Option group
+            'maint_display_text', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'setting_section_id', // ID
+            'Maintenance Mode Settings', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'maintenance_mode-setting-admin' // Page
+        );  
+
+        add_settings_field(
+            'maint_title', 
+            'Page Title', 
+            array( $this, 'maint_title_callback' ), 
+            'maintenance_mode-setting-admin', 
+            'setting_section_id'
+        );      
+
+        add_settings_field(
+            'maint_text', // ID
+            'Display Message', // Title 
+            array( $this, 'maint_text_callback' ), // Callback
+            'maintenance_mode-setting-admin', // Page
+            'setting_section_id' // Section           
+        );      
+
+        
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        if( !empty( $input['maint_text'] ) )
+            $input['maint_text'] = sanitize_text_field( $input['maint_text'] );
+       if( !empty( $input['maint_title'] ) )
+            $input['maint_title'] = sanitize_text_field( $input['maint_title'] );
+
+        return $input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function maint_text_callback()
+    {
+
+        echo '<textarea id="maint_text" name="maint_display_text[maint_text]" cols="100">'. $this->options['maint_text'] . '</textarea>';
+        /*'<input type="text" id="maint_text" name="maint_display_text[maint_text]" value="'. $this->options['maint_text']. '" />';*/
+    }
+    public function maint_title_callback()
+    {
+
+        echo '<input type="text" id="maint_title" name="maint_display_text[maint_title]" value="'. $this->options['maint_title']. '" size=100/>';
+    }
+    // Define default option settings
+}
+
+if( is_admin() )
+    $maintmode_settings_page = new MaintenanceModeSettingsPage();
---
+++ mainenance-mode-preview-link/maintenance-mode-preview-link.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Plugin Name: Maintenance Mode + Preview Link
+Plugin URI: 
+Description: Adds a Maintenance Mode Screen for WordPress with an override preview parameter
+Version: 1.0
+Author: Jonathan Wold & Natalie Ebbens
+Author URI: http://jonathanwold.com
+License: GPL2
+Copyright 2013 Jonathan Wold
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+/* Maintenance Mode + Preview Link */
+include('MaintenanceModeSettingsPage.php');
+function jw_maintenance_mode() {
+    //if ( !current_user_can( 'edit_themes' ) || !is_user_logged_in() ) {
+    $options = get_option( 'maint_display_text' );
+    $message = $options['maint_text'];
+    $title = $options['maint_title'];
+    if( !empty( $_GET ) ) {
+                                        
+        // First, grab the current string and parse it out.
+        parse_str( $_SERVER['QUERY_STRING'], $query_string );
+               
+/*
+        echo "<pre>";
+        print_r('sess:'.$_SESSION);
+        print_r('cookie:'.$_COOKIE);
+        print_r('post:'.$_POST);
+        echo "</pre>";
+*/
+        if(isset($query_string['nopreview'])){
+           setcookie('preview', '', time()-60000, COOKIEPATH, COOKIE_DOMAIN, false);
+            wp_die($message,$title); 
+        }
+
+        if ( !isset($query_string['preview']) and !isset($_COOKIE['preview'] )  ) {
+            wp_die($message,$title); 
+        }
+        elseif (isset($query_string['preview']) and !isset($_COOKIE['preview'] ) ){
+            //set the cookie
+            setcookie('preview', 1, time()+60*60*24*365, COOKIEPATH, COOKIE_DOMAIN, false);
+        }
+        
+    }
+    elseif (!isset($_COOKIE['preview'] ) ){
+            wp_die($message,$title); 
+    }
+}
+add_action('get_header', 'jw_maintenance_mode');
+if (function_exists('jw_maintenance_mode') ) {
+	function my_admin_notice(){
+	$url = admin_url( 'plugins.php?plugin_status=active');
+	echo '<div class="updated">
+	<p>Maintenance Mode is activated. Please don\'t forget to <a href="' . $url . '">deactivate</a> it as soon as you are done.</p>	
+	</div>';
+}
+add_action('admin_notices', 'my_admin_notice');
+}
+//set defaults
+register_activation_hook(__FILE__, 'add_maint_defaults_fn');
+function add_maint_defaults_fn() {
+    //set defaults if needed
+    $tmp = get_option('maint_display_text');
+    if(!is_array($tmp)) {
+        $arr = array("maint_text" => "<h2>Temporarily Offline</h2><h3>Hey there! Our site is temporarily offline.</h3>",
+                     "maint_title" => "Temporarily Unavailable");
+        update_option('maint_display_text', $arr);
+    }
+}
+
+
+
+/* 
+== Change Log == 
+*/
+?>
